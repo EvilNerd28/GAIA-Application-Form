51debcb75e1399ecd1aa97387e7359fbc15caf41
Ketaki Shinde <114794967+KetakiShinde28@users.noreply.github.com>
2024-03-08 15:33:10 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -9,3 +9,31 @@ document.getElementById("camera-input").addEventListener("change", function(even
     var content = document.querySelector('.content');
     content.style.maxHeight = content.style.maxHeight === '200px' ? null : '200px'; // Adjust the height accordingly
 });
+
+function toggleDropdown() {
+  var dropdown = document.getElementById("kycDropdown");
+  dropdown.classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.highlight')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      for (var i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show')) {
+              openDropdown.classList.remove('show');
+          }
+      }
+  }
+}
+
+function redirectOnChange() {
+  var selectElement = document.getElementById("customerType");
+  var selectedValue = selectElement.options[selectElement.selectedIndex].value;
+
+  if (selectedValue === "New-Application") {
+      window.location.href = "new.html";
+  }
+  // Add other cases if needed
+}
