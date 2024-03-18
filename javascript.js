2d32ae28dbbe239b9908cea73fdd752122758b7a
Ketaki Shinde <114794967+KetakiShinde28@users.noreply.github.com>
2024-03-18 15:08:24 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,12 +1,71 @@
+/* NOTES:
+ * The two functions below have been commented out as they call things that have not been initialized yet
+ * Make sure the script is run at the end of the body, this will at least make sure everything is initialized before it is called
+ * The functions have been replaced by functions with onclicks instead (bad process, simple design), as shown below
+ * Finish view secondary and view KYC to actually implement the wanted functionality
+*/
+
+/* REMOVED:
 document.getElementById("camera-input").addEventListener("change", function(event) {
     // Process captured image here (preview, upload to server)
     const file = event.target.files[0];
-  });
-
-  document.getElementById('viewMoreBtn').addEventListener('click', function() {
-    var content = document.querySelector('.content');
-    content.style.maxHeight = content.style.maxHeight === '200px' ? null : '200px'; // Adjust the height accordingly
 });
+
+document.getElementById('viewMoreBtn').addEventListener('click', function() {
+  //console.log("ran view more")
+  var content = document.querySelector('.content');
+  content.style.maxHeight = content.style.maxHeight === '200px' ? null : '200px'; // Adjust the height accordingly
+});
+*/
+
+// Function to handle clicking on the lens image for Secondary documentation
+function viewSecondary() {
+  // TODO: display secondary documentation
+  console.log("Secondary documentation should be viewed"); // comment out when done
+}
+
+// Function to handle clicking on the lens image for KYC documentation
+function viewKYC() {
+  // TODO: display KYC
+  console.log("KYC should be viewed"); // comment out when done
+}
+
+// Function to show tooltip on hover for lens images
+function showTooltip(element) {
+  const dataInfo = element.dataset.info;
+  const tooltip = document.createElement('div');
+  tooltip.classList.add('tooltip');
+  tooltip.textContent = dataInfo;
+
+  const tooltipLeft = element.offsetLeft + element.clientWidth + 10;
+  const tooltipTop = element.offsetTop;
+
+  tooltip.style.left = `${tooltipLeft}px`;
+  tooltip.style.top = `${tooltipTop}px`;
+
+  document.body.appendChild(tooltip);
+
+  // Store reference to tooltip in the element itself
+  element.hoverTooltip = tooltip;
+}
+
+// Function to hide tooltip when mouse moves out for lens images
+function hideTooltip() {
+  const tooltips = document.querySelectorAll('.tooltip');
+  tooltips.forEach(tooltip => tooltip.remove());
+}
+
+// Remove tooltip when lens image is clicked
+document.addEventListener('click', function(event) {
+  if (event.target.classList.contains('hover-info') && event.target.hoverTooltip) {
+    event.target.hoverTooltip.remove();
+  }
+});
+
+
+function testFunc(){
+  console.log("it works")
+}
 
 function toggleDropdown() {
   var dropdown = document.getElementById("kycDropdown");
@@ -70,7 +129,6 @@ function applyHoverInfoBehavior() {
 window.addEventListener('DOMContentLoaded', applyHoverInfoBehavior);
 
 
-
 //VIEW DOCUMENT (LENS)
 
 var modal = document.getElementById("myModal");
@@ -87,7 +145,6 @@ function openModal() {
 function closeModal() {
   modal.style.display = "none";
 }
-
 
 
 
